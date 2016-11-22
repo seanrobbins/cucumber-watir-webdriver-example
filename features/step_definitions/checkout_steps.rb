af5913b870d091f64673e05a617eee4a94380cc2
Sean Robbins <serobbins@expedia.com>
2016-11-22 23:22:08 +0000

    Passing checkout test

--- features/step_definitions/checkout_steps.rb
+++ features/step_definitions/checkout_steps.rb
@@ -12,5 +12,24 @@ Given(/^I have some items in my basket$/) do
 end
 
 Then(/^I can successfully checkout$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  @browser.link(:text => "Checkout").wait_until_present
+  @browser.link(:text => "Checkout").click
+  
+  @browser.button(:value => "Place Order").wait_until_present 
+  @browser.text_field(:id => "accountEmail").set "test@testing.com"
+  @browser.text_field(:id => "accountPhone").set "0123456789"
+  @browser.text_field(:id => "shipAddress").set "1 Test Place"
+  @browser.text_field(:id => "shipCity").set "Testville"
+  @browser.text_field(:id => "shipState").set "TN"
+  @browser.text_field(:id => "shipZip").set "34567"
+  @browser.select_list(:id => "shipCountry").select_value "US" 
+  @browser.text_field(:id => "ccName").set "Mr T Tester"
+  @browser.text_field(:id => "ccNumber").set "4111111111111111"
+  @browser.select_list(:id => "ccExpMonth").select_value "01" 
+  @browser.select_list(:id => "ccExpYear").select_value Time.now.year + 1
+  @browser.text_field(:id => "ccCVV").set "111"
+  @browser.button(:value => "Place Order").click 
+
+  @browser.link(:text => "Finish").wait_until_present
+  expect(@browser.url).to eq("https://shop.polymer-project.org/checkout/success")
 end
